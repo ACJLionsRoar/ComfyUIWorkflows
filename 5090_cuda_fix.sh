6818221a0e83579c795e7494019fc25b816c9961
ACJLionsRoar <32523737+ACJLionsRoar@users.noreply.github.com>
2025-06-23 13:57:24 +0530

    run this if running on 5090 for cuda fix

---
+++ 5090_cuda_fix.sh
@@ -0,0 +1,14 @@
+# This fixes a CUDA error for wan21local_install.sh
+# CUDA_LAUNCH_BLOCKING=1.Compile with TORCH_USE_CUDA_DSA to enable device-side assertions.
+
+pip install setuptools --upgrade
+
+pip install ninja cmake packaging
+
+pip uninstall -y torch torchvision torchaudio xformers || true
+
+rm -rf ~/.cache/torch_extensions
+
+pip install --force-reinstall torch==2.7.1+cu128 torchvision==0.22.1+cu128 --extra-index-url https://download.pytorch.org/whl/cu128
+
+export TORCH_CUDA_ARCH_LIST="9.0"
