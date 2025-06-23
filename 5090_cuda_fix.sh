6bf2640492325e9ec5d5bf99a5e7151832dbbbfd
ACJLionsRoar <32523737+ACJLionsRoar@users.noreply.github.com>
2025-06-23 14:16:08 +0530

    added post link

--- 5090_cuda_fix.sh
+++ 5090_cuda_fix.sh
@@ -12,3 +12,7 @@ rm -rf ~/.cache/torch_extensions
 pip install --force-reinstall torch==2.7.1+cu128 torchvision==0.22.1+cu128 --extra-index-url https://download.pytorch.org/whl/cu128
 
 export TORCH_CUDA_ARCH_LIST="9.0"
+
+
+#link to reddit post with solution
+#https://www.reddit.com/r/StableDiffusion/comments/1jco4kw/rtx_5090_help_with_cuda_error_no_kernel_image_is/?tl=hi-latn
